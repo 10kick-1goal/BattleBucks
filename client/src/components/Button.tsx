79ad1e12e157c6c6392ad9f4e2485ad33d3df068
spinzed <davor@gmail.com>
2024-07-10 12:32:00 +0200

    Add colorful borders to ceratin buttons

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -8,12 +8,20 @@ interface ButtonProps {
   style?: CSSProperties;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   className?: string;
+  colorfulBorder?: boolean;
 }
 
 function Button(props: ButtonProps) {
+  let baseClass = props.type === "big" ? "bigButton" : "button";
+  if (props.colorfulBorder) baseClass += " colorfulBorder";
+  if (props.className) baseClass += " " + props.className;
   return (
-    <button className={(props.type === "big" ? "bigButton" : "button")  + (props.className ? " " + props.className : "")} style={props.style} onClick={props.onClick}>
-      <div style={{ fontWeight: "bold" }}>{props.children}</div>
+    <button className={baseClass} style={props.style} onClick={props.onClick}>
+      <div className="buttonInner">
+        <div>
+          {props.children}
+        </div>
+      </div>
     </button>
   );
 }
