a8dcf72a62bd35e9756e3b7c6ffb9f690ef193f3
spinzed <davor@gmail.com>
2024-07-22 22:20:00 +0200

    Add more animations in versus screen

--- client/src/views/Versus/Versus.tsx
+++ client/src/views/Versus/Versus.tsx
@@ -1,18 +1,37 @@
 import Rock from "../../assets/rock2.png";
 import Paper from "../../assets/paper2.png";
 import Scissors from "../../assets/scissors2.png";
-import "./Versus.scss"
 import Button from "../../components/Button/Button";
 import Avatar from "../../components/Avatar";
+import { AnimationDefinition, motion, useAnimationControls } from "framer-motion";
 import { CSSProperties, useState } from "react";
+import "./Versus.scss"
 
 type Item = "rock" | "paper" | "scissors";
+
+const images = {
+  "rock": Rock,
+  "paper": Paper,
+  "scissors": Scissors,
+};
 
 const transformIdleBase = "translateY(11em) ";
 const transformSelected = "translateY(1em) scale(1.5)";
 const transformSelectedFinal = "translateY(1em) scale(1.2)";
 const transformSelectedLeft = "translateY(1em) translateX(-5em) scale(1.2)";
 const transformSelectedRight = "translateY(1em) translateX(5em) scale(1.2)";
+
+const initialOpponentPosition: AnimationDefinition = {
+  opacity: 0,
+  translateY: "-5em",
+  transition: { duration: 0.3, ease: "easeOut" },
+};
+
+const finalOpponentPosition: AnimationDefinition = {
+  opacity: 1,
+  translateY: 0,
+  transition: { duration: 0.3, ease: "easeOut" },
+};
 
 enum GameState {
   IDLE,
@@ -26,10 +45,25 @@ const transforms = {
 }
 
 function Versus() {
+  const controls = useAnimationControls();
+
   const [selectedItem, setSelectedItem] = useState<Item>();
   const [state, setState] = useState<GameState>(GameState.IDLE);
 
   const isChosen = state !== GameState.IDLE;
+
+  function commenceBattle() {
+    if (!state == GameState.CHOSEN) return;
+    setState(GameState.BATTLE);
+    console.log("weee")
+    controls.start(finalOpponentPosition);
+  }
+
+  function reset() {
+    setState(GameState.IDLE);
+    setSelectedItem(undefined);
+    controls.start(initialOpponentPosition);
+  }
 
   return (
     <div className="flexCol flex" style={{ margin: "1em" }}>
@@ -48,14 +82,21 @@ function Versus() {
       <div className="flexRow center" style={{ flex: 5 }}>
         <div className="table"></div>
       </div>
-      <div className="items" style={{ transform: "translateY(0)" }}>
-        <Item state={state} selected={selectedItem === "rock"} transformIdle={transformIdleBase + " translateX(-110%)"} name="rock" className="rock" img={Rock} onClick={() => !isChosen && setSelectedItem("rock")} />
-        <Item state={state} selected={selectedItem === "paper"} transformIdle={transformIdleBase + " translateX(0%)"} name="paper" className="paper" img={Paper} onClick={() => !isChosen && setSelectedItem("paper")} />
-        <Item state={state} selected={selectedItem === "scissors"} transformIdle={transformIdleBase + " translateX(110%)"} name="scissors" className="scissors" img={Scissors} onClick={() => !isChosen && setSelectedItem("scissors")} />
+      <div className="items">
+        <Item transform={selectedItem === "rock" ? transforms[state] : transformIdleBase + " translateX(-110%)"} type="rock" onClick={() => !isChosen && setSelectedItem("rock")} />
+        <Item transform={selectedItem === "paper" ? transforms[state] : transformIdleBase + " translateX(0%)"} type="paper" onClick={() => !isChosen && setSelectedItem("paper")} />
+        <Item transform={selectedItem === "scissors" ? transforms[state] : transformIdleBase + " translateX(110%)"} type="scissors" onClick={() => !isChosen && setSelectedItem("scissors")} />
+        <motion.div
+          animate={controls}
+          style={initialOpponentPosition}
+          className="flex center"
+        >
+          <Item transform={transformSelectedRight} type="scissors" onClick={() => !isChosen && setSelectedItem("scissors")} />
+        </motion.div>
       </div>
       <div className="flexRow flex center" style={{ justifyContent: "flex-end" }}>
-        <Button type="cancel" onClick={() => { setState(GameState.BATTLE); }}>TEMP battle</Button>
-        <Button type="cancel" onClick={() => { setSelectedItem(undefined); setState(GameState.IDLE); }}>TEMP reset</Button>
+        <Button type="cancel" onClick={commenceBattle}>TEMP battle</Button>
+        <Button type="cancel" onClick={reset}>TEMP reset</Button>
         <Button type="accept" disabled={isChosen} onClick={() => !!selectedItem && setState(GameState.CHOSEN)}>Choose</Button>
       </div>
     </div>
@@ -63,26 +104,16 @@ function Versus() {
 }
 
 interface ItemProps {
-  name: string;
-  img: string;
-  className: string;
+  type: Item;
   onClick: React.MouseEventHandler<HTMLDivElement>;
-  selected: boolean;
-  state: GameState;
-  transformIdle: string;
+  transform: string;
 }
 
 function Item(props: ItemProps) {
-  const transform = props.selected ? transforms[props.state] : props.transformIdle;
-
   return (
-    <div
-      onClick={props.onClick}
-      className={"item " + props.className}
-      style={{ transform }}
-    >
-      <img src={props.img} className="rps" alt={props.name} />
-      <div style={{ textTransform: "uppercase" }}>{props.name}</div>
+    <div onClick={props.onClick} className={"item " + props.type} style={{ transform: props.transform }}>
+      <img src={images[props.type]} className="rps" alt={props.type} />
+      <div style={{ textTransform: "uppercase" }}>{props.type}</div>
     </div>
   );
 }
