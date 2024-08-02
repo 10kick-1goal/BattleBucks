97e6bfdd6aa16093ad898e2b913b4cdc405ec48c
10kick-1goal <kick.3top@gmail.com>
2024-08-02 17:08:03 +0900

    feat: refine user auth

---
+++ server/src/api/middlewares/index.ts
@@ -0,0 +1,50 @@
+import { TRPCError } from "@trpc/server";
+import { t } from "../../app";
+import CryptoJS from "crypto-js";
+import { z } from "zod";
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+if (!TELEGRAM_BOT_TOKEN) {
+  throw new Error("TELEGRAM_BOT_TOKEN environment variable is not set");
+}
+
+export const isAuthenticated = t.middleware(async ({ next, ctx }) => {
+  const initData = new URLSearchParams(
+    ctx.req.get("telegramInitData") as string | undefined
+  );
+  const hash = initData.get("hash");
+  let isAuth;
+  let dataToCheck: string[] = [];
+
+  initData.sort();
+  initData.forEach(
+    (val, key) => key !== "hash" && dataToCheck.push(`${key}=${val}`)
+  );
+
+  const secret = TELEGRAM_BOT_TOKEN
+    ? CryptoJS.HmacSHA256(TELEGRAM_BOT_TOKEN, "WebAppData")
+    : null;
+  const _hash = CryptoJS.HmacSHA256(
+    dataToCheck.join("\n"),
+    secret ?? ""
+  ).toString(CryptoJS.enc.Hex);
+
+  isAuth = _hash === hash;
+  if (isAuth) {
+    return next({
+      ctx: {
+        ...ctx,
+        user: {
+          isLoggedIn: isAuth,
+        },
+      },
+    });
+  } else {
+    throw new TRPCError({ code: "BAD_REQUEST", message: "Not authorized" });
+  }
+});
+
+export const loggerMiddleware = t.middleware(async ({ path, type, next }) => {
+  console.log(`${new Date().toISOString()} - ${type} - ${path}`);
+  return next();
+});
--- server/src/api/router.ts
+++ server/src/api/router.ts
@@ -1,39 +1,3 @@
-// import { z } from "zod";
-
-// import { publicProcedure, router } from "./index";
-// import { createUser } from "./createUser";
-// import { newMatch } from "./newMatch";
-// import { gamePlay } from "./gamePlay";
-// import { updateWinner } from "./updateWinner";
-// import { searchPlayer } from "./searchPlayer";
-// import { getProfile } from "./getProfile";
-
-// // combined router
-// export const appRouter = router({
-//     test: publicProcedure
-//         .input(
-//             z.object({
-//                 hello: z.string(),
-//             })
-//         ).output(z.string())
-//         .query((req) => {
-//             const helloValue = req.input.hello; // string
-//             console.log(helloValue);
-//             return helloValue;
-//         }),
-//     createUser: createUser,
-//     newMatch: newMatch,
-//     getProfile: getProfile,
-//     searchPlayer: searchPlayer,
-//     // gamePlay: gamePlay,
-//     // updateWinner: updateWinner,
-//     // sample: sampleRouter,
-// });
-
-// // type definition of trpc API
-// export type AppRouter = typeof appRouter;
-
-
 import { router } from "./index";
 import gameRouter from "./routes/game";
 import userRouter from "./routes/user";
