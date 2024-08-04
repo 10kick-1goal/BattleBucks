c2fc8393f230863c703807dfac32b0336d9e9cb6
spinzed <davor@gmail.com>
2024-08-04 17:49:02 +0200

    Update animations on game join and fix a lot of small css issues

--- client/src/views/VersusLobby.tsx
+++ client/src/views/VersusLobby.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router";
 import Button from "../components/Button/Button";
-import viteLogo from "/vite.svg";
 import Logo from "../components/Logo/Logo";
 
 function VersusLobby() {
@@ -10,8 +9,8 @@ function VersusLobby() {
     <div className="flexCol flex" style={{ margin: "5em 1em" }}>
       <Logo />
       <div className="flexRow" style={{ margin: "2em 0", gap: "1em"}}>
-        <Button type="big" className="flex" onClick={() => navigate("/vs/buyin")}><b>Find a Match</b></Button>
-        <Button type="big" className="flex"><b>Invite a Friend</b></Button>
+        <Button type="big" className="flex" onClick={() => navigate("/vs/buyin")}>Find a Match</Button>
+        <Button type="big" className="flex">Invite a Friend</Button>
       </div>
       <Button type="cancel" onClick={() => navigate(-1)}>Back</Button>
     </div>
