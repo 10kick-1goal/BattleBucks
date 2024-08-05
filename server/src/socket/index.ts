aaaf71eb03c39f2c536e1c60c46bba1b22556b9f
10kick-1goal <kick.3top@gmail.com>
2024-08-06 04:21:52 +0900

    feat: update socket apis

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -3,14 +3,14 @@ import { gameEvents } from "./events/gameEvents";
 import { userEvents } from "./events/userEvents";
 
 const setupSocket = (io: Server) => {
-  io.use((socket, next) => {
-    const token = socket.handshake.auth.token;
-    if (token === process.env.SOCKET_TOKEN) {
-      next();
-    } else {
-      next(new Error("Authentication error"));
-    }
-  });
+  // io.use((socket, next) => {
+  //   const token = socket.handshake.auth.token;
+  //   if (token === process.env.SOCKET_TOKEN) {
+  //     next();
+  //   } else {
+  //     next(new Error("Authentication error"));
+  //   }
+  // });
 
   io.on("connection", (socket) => {
     console.log("A user connected");
@@ -25,14 +25,4 @@ const setupSocket = (io: Server) => {
   });
 };
 
-// Configure Socket.IO
-const io = new Server({
-  cors: {
-    origin:
-      process.env.NODE_ENV === "production" ? process.env.ALLOWED_ORIGINS : "*",
-  },
-});
-
-setupSocket(io);
-
-export { io };
+export { setupSocket };
