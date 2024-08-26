cc941f7e3555155012f1731ce0993d3a3f691779
10kick-1goal <kick.3top@gmail.com>
2024-08-27 03:11:21 +0900

    fix: user disconnection situation, friend table data fix

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -1,9 +1,10 @@
-import { Server, Socket } from "socket.io";
-import { gameEvents } from "./events/gameEvents";
-import { userEvents } from "./events/userEvents";
-import { friendEvents } from "./events/friendEvents";
-import jwt from "jsonwebtoken";
-import { userStatus } from "./userStatus";
+import { Server, Socket } from 'socket.io';
+import { gameEvents } from './events/gameEvents';
+import { userEvents } from './events/userEvents';
+import { friendEvents } from './events/friendEvents';
+import jwt from 'jsonwebtoken';
+import { userStatus } from './userStatus';
+import { prisma } from '../prisma';
 
 // Extend the Socket interface
 interface CustomSocket extends Socket {
@@ -15,19 +16,15 @@ const setupSocket = (io: Server) => {
   io.use((socket: CustomSocket, next) => {
     const token = socket.handshake.headers.token as string;
     if (token) {
-      jwt.verify(
-        token,
-        process.env.JWT_SECRET || "",
-        (err: any, decoded: any) => {
-          socket.user = decoded;
-          next();
-        }
-      );
+      jwt.verify(token, process.env.JWT_SECRET || '', (err: any, decoded: any) => {
+        socket.user = decoded;
+        next();
+      });
     } else next();
   });
 
-  io.on("connection", (socket: CustomSocket) => {
-    userStatus[socket.id] = { status: "ONLINE", userId: socket.user?.userId };
+  io.on('connection', (socket: CustomSocket) => {
+    userStatus[socket.id] = { status: 'ONLINE', userId: socket.user?.userId };
     socket.join(socket.user?.userId);
 
     console.log(userStatus);
@@ -36,8 +33,32 @@ const setupSocket = (io: Server) => {
     userEvents(socket, io);
     friendEvents(socket, io);
 
-    socket.on("disconnect", (reason) => {
-      console.log(userStatus[socket.id], "goes offline...")
+    socket.on('disconnect', async (reason) => {
+      console.log(userStatus[socket.id], 'goes offline...', reason);
+      try {
+        const userId = userStatus[socket.id].userId;
+        const games = await prisma.gameParticipant.findMany({
+          where: { playerId: userId },
+        });
+
+        for (const game of games) {
+          if (userId)
+            await prisma.gameParticipant.delete({
+              where: {
+                gameId_playerId: {
+                  gameId: game.gameId,
+                  playerId: userId,
+                },
+              },
+            });
+
+          io.to(game.gameId).emit('S2C_PLAYER_LEFT', {
+            playerId: userId,
+          });
+        }
+      } catch (error) {
+        console.error(`Failed to remove player on disconnect:`, error);
+      }
       delete userStatus[socket.id];
     });
   });
